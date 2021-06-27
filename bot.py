158f2ce91103b48c1a9dee35aa554fd2ed48a937
roctbb <roctbb@gmail.com>
2021-06-27 18:48:08 +0300

    добавили возможность простудиться

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import random
+
 import config
 import importlib
 from init import *
@@ -12,6 +14,11 @@ def life_support():
             # еда
             if user['eat_points'] != 0:
                 user['eat_points'] -= 1
+
+            # заболевание
+            if "wet" in user['states'] and "sick" not in user['states'] and random.randint(1, 20) == 1:
+                bot.send_message(user['chat_id'], "Вы простудились...")
+                user['states'].append('sick')
 
         for location in locations:
             try:
