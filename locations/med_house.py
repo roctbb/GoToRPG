f3b6f2319c74ceb06f7873f365fe95c6627e5cad
macbook <kathianikina@gmail.com>
2021-06-27 13:53:29 +0300

    Начинаем работу над медпунктом

--- locations/med_house.py
+++ locations/med_house.py
@@ -6,12 +6,14 @@ def message(msg, user, location, neighbors, bot):
     if "/skiplesson" in msg.text:
         if 10 < hour < 11.30:
             bot.send_message(user["chat_id"], "Вы пожаловались на плохое самочувсвие и остались у Лены")
-        elif 12 < hour < 13.30:
+        elif 12 < hour < 14:
             bot.send_message(user["chat_id"], "Вы пожаловались на плохое самочувствие и остались у Лены.")
         elif 15 < hour < 16.30:
             bot.send_message(user["chat_id"], "Вы пожаловались на плохое самочувствие и остались у Лены.")
+       elif 17 < hour < 19
+           bot.send_message(user["chat_id"], "Вы пожаловались на плохое самочувствие и остались у Лены.")
         else:
-            bot.send_message(user["chat_id"], "Вы прилегли отдохнуть.")
+            bot.send_message(user["chat_id"], "Пар нету, а значит и прогуливать нечего)")
     else:
         for neighbor in neighbors:
             if neighbor["chat_id"] != user["chat_id"]:
