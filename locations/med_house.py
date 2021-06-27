ea14e6255520c4af3f5b2ce595c1239ba2dd2b75
macbook <kathianikina@gmail.com>
2021-06-27 16:17:43 +0300

    Начинаем работу над медпунктом

--- locations/med_house.py
+++ locations/med_house.py
@@ -21,8 +21,7 @@ def message(msg, user, location, neighbors, bot):
         else:
             bot.send_message(user["chat_id"], "Сейчас можно гулять и без этого)")
     if "/heal" in msg.text:
-    else:
-        bot.send_message
+        bot.send_message(user[])
         for neighbor in neighbors:
             if neighbor["chat_id"] != user["chat_id"]:
                 bot.send_message(neighbor["chat_id"], "{}: {}".format(user["name"], msg.text))
