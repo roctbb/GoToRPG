de7dcc86962f51e6d8d6b156cf9d7b3d42566511
User <regoeymelnik@gmail.com>
2021-06-27 18:54:33 +0300

    Вроде бы дом охотника

--- locations/hunter_house.py
+++ locations/hunter_house.py
@@ -1,18 +1,12 @@
-
 @bot.message_handler(commands=['Vzyat_kapitoshku'])
-if "/waterballгз" in msg.text:
-    user['inventory'].append(kapitoshka)
-    for neighbor in neighbors:
-        bot.send_message(neighbor['chat_id'], "{} поднял капитошку")
+def huntho(message):
+    if "/waterballгз" in msg.text:
+        user['inventory'].append(kapitoshka)
+        for neighbor in neighbors:
+            bot.send_message(neighbor['chat_id'], "{} поднял капитошку")
 
 def event(users, location, bot):
-
-    hour = datetime.now().hour
-
-    # пояление Николая
-    if 0 < hour < 7:
-        if random.randint(1, 3) == 1:
-            for user in users:
-                bot.send_sticker(user['chat_id'], stickers['nikolay'])
-                bot.send_message(user['chat_id'], 'В доме охотника появился Николай. Вы отвлекаете его от работы! Вы наказаны!')
-                user['states'].append('punishment')
+    for user in users:
+        bot.send_sticker(user['chat_id'], stickers['nikolay'])
+        bot.send_message(user['chat_id'], 'На улице появился Николай. Вы отвлекаете его от работы! Вы наказаны!')
+        user['states'].append('punishment')
