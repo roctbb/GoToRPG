dbd471cb10e210adee6c4f9611c570559ac69879
Deniz <denistuncbilek92718@gmail.com>
2021-06-27 18:01:24 +0300

    кростель

--- locations/korostel.py
+++ locations/korostel.py
@@ -1,19 +1,19 @@
 from datetime import datetime
 
-def message(msg, user, location, neighborsr, bot):
+def message(msg, user, location, neighbors, bot):
 
     hour = datetime.now().hour
     if "/breakfast" in msg.text:
-        if 9< hour < 11:
+        if 9 < hour < 11:
           bot.send_message(user["chat_id"], "Вы завтракаете ")
         else:
          bot.send_message(user["chat_id"], "Вы сейчас не можете кушац")
-        if "/lunch" in msg.text:
+    if "/lunch" in msg.text:
             if 14 < hour < 15:
                 bot.send_message(user["chat_id"], "Вы обедаете ")
             else:
                 bot.send_message(user["chat_id"], "Вы сейчас не можете кушац")
-        if "/dinner" in msg.text:
+    if "/dinner" in msg.text:
             if 19 < hour < 20:
                 bot.send_message(user["chat_id"], "Вы ужинаете ")
             else:
