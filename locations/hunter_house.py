e453153ac0de8d3f950e2ad697e3164999a37a72
User <regoeymelnik@gmail.com>
2021-06-27 18:10:54 +0300

    Вроде бы дом охотника

--- locations/hunter_house.py
+++ locations/hunter_house.py
@@ -1,6 +1,6 @@
-users=[]
+
 @bot.message_handler(commands=['Vzyat_kapitoshku'])
-if "/waterball" in msg.text:
-    user['inventory'].append kapitoshka
+if "/waterballгз" in msg.text:
+    user['inventory'].append(kapitoshka)
     for neighbor in neighbors:
         bot.send_message(neighbor['chat_id'], "{} поднял капитошку")
