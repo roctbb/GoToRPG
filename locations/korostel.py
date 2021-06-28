0e0b8322a8d9b150c55b3e600d2136d5a89897a4
Deniz <denistuncbilek92718@gmail.com>
2021-06-28 16:19:10 +0300

    кростель

--- locations/korostel.py
+++ locations/korostel.py
@@ -12,7 +12,7 @@ def welcome (user,location,bot):
 Напишите /bread чтобы  купить хлеб.
 Аккуратнее с перееданием!"""
     if 11 <= hour <= 13:
-        welcome_text = "Сейчас нельзя покушать,можете купить хлеб."
+        welcome_text = "Сейчас нельзя покушать,можете купить хлеб, напишите /bread чтобы купить хлеб."
     if 13 <= hour <= 14:
         welcome_text = """Это коростель здесь вы можете покушать.
     /lunch-пообедать.
@@ -21,12 +21,14 @@ def welcome (user,location,bot):
 
 
     if 14 <= hour <= 19:
-        welcome_text = "Сейчас нельзя покушать, можете купить хлеб."
+        welcome_text = "Сейчас нельзя покушать, можете купить хлеб, напишите /bread чтобы купить хлеб."
     if 19 <= hour <= 20:
         welcome_text = """Это коростель здесь вы можете покушать.
     /dinner-поожинать.
     Напишите /bread чтобы  купить хлеб.
     Аккуратнее с перееданием!"""
+    if 20 <= hour <= 10:
+        welcome_text = "Сейчас нельзя покушать, можете купить хлеб, напишите /bread чтобы купить хлеб."
     bot.send_message(user["chat_id"], welcome_text)
 
 def message(msg, user, location, neighbors, bot):
@@ -72,12 +74,13 @@ def message(msg, user, location, neighbors, bot):
             bot.send_message(user["chat_id"], 'Вы купили один хлеб тобы сьесть напишите /eat_bread')
             user['inventory'].remove('coin')
             user['inventory'].append('bread')
-        else:
-            bot.send_message(user["chat_id"], "У вас нет coin")
             if user["eat_points"] > 100:
                 bot.send_message(user["chat_id"], "У вас понос")
                 if "ponos" not in user['states']:
                     user['states'].append("ponos")
+        else:
+            bot.send_message(user["chat_id"], "У вас нет coin")
+
     if "/eat_bread" in msg.text:
         if 'bread' in user ['inventory']:
             bot.send_message(user["chat_id"], 'Вы сьели хлеб')
