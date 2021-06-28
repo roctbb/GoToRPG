4ad5b8de7582bfd42bd1cd2aff5063f9b3289d46
roctbb <roctbb@gmail.com>
2021-06-28 13:27:05 +0300

    добавил описание и смайлики на улицу

--- locations/street.py
+++ locations/street.py
@@ -1,7 +1,21 @@
+import importlib
 from datetime import datetime
 import random
 
 from init import *
+
+def welcome(user, location, bot):
+    hour = datetime.now().hour
+
+    if 0 < hour < 7:
+        bot.send_message(user['chat_id'], "🌚 Вы вышли прогуляться на улицу. Сейчас ночь и находиться вне домика нельзя, остерегайтесь Николая! Если вас поймают, то вас накажут и вы расстроитесь.\n\n"
+                                          "* /waterball ПОЛЬЗОВАТЕЛЬ - бросить капитошку;\n"
+                                          "* /sleep - спать.")
+    else:
+        bot.send_message(user['chat_id'],
+                         "🌞 Вы вышли прогуляться на улицу.\n\n"
+                         "* /waterball ПОЛЬЗОВАТЕЛЬ - бросить капитошку;\n"
+                         "* /sleep - спать.")
 
 def message(msg, user, location, neighbors, bot):
     hour = datetime.now().hour
@@ -39,14 +53,14 @@ def message(msg, user, location, neighbors, bot):
 
             if random.randint(1, 10) > border:
                 for neighbor in neighbors:
-                    bot.send_message(neighbor['chat_id'], "{} кидает капитошку в {} и попадает!".format(user['name'], target['name']))
-                bot.send_message(target['chat_id'], "Вы намокли!")
+                    bot.send_message(neighbor['chat_id'], "🎯{} кидает капитошку в {} и попадает!".format(user['name'], target['name']))
+                bot.send_message(target['chat_id'], "💦 В вас попали капитошкой и вы намокли!")
 
                 if "wet" not in target['states']:
                     target['states'].append('wet')
             else:
                 for neighbor in neighbors:
-                    bot.send_message(neighbor['chat_id'], "{} кидает капитошку в {}, но промахивается!".format(user['name'], target['name']))
+                    bot.send_message(neighbor['chat_id'], "👀 {} кидает капитошку в {}, но промахивается!".format(user['name'], target['name']))
 
             user['inventory'].remove('waterball')
 
@@ -57,32 +71,35 @@ def message(msg, user, location, neighbors, bot):
 
     if "/sleep" in msg.text:
         if 10 < hour < 15:
-            bot.send_message(user["chat_id"], "Вы загораете.")
+            bot.send_message(user["chat_id"], "🥵 Сейчас слишком жарко уснуть не выходит. Вы лежите на солнце и получаете солнчный удар.")
+            if "sick" not in user['states']:
+                user['states'].append('sick')
         elif 0 < hour < 7:
-            bot.send_message(user["chat_id"], "Вы спите на улице.")
-            user['sleep_points'] += 20
+            if "sick" not in user['states']:
+                bot.send_message(user["chat_id"], "💤 Вы спите на улице.")
+                user['sleep_points'] += 20
 
-            if ""
+                if random.randint(1,3) == 1:
+                    bot.send_message(user["chat_id"], "🤒 Тут холодно, вы простудились.")
+                    user['states'].append('sick')
+            else:
+                bot.send_message(user["chat_id"], "🤒 Вы простужены и не можете спать на улице.")
         else:
-            bot.send_message(user["chat_id"], "Вы прилегли отдохнуть.")
-    else:
-        for neighbor in neighbors:
-            if neighbor["chat_id"] != user["chat_id"]:
-                bot.send_message(neighbor["chat_id"], "{}: {}".format(user["name"], msg.text))
-        return
+            bot.send_message(user["chat_id"], "💤 Вы прилегли отдохнуть.")
+            user['sleep_points'] += 5
 
     for neighbor in neighbors:
         if neighbor["chat_id"] != user["chat_id"]:
             bot.send_message(neighbor["chat_id"], "{}: {}".format(user["name"], msg.text))
 
-
-
 def event(users, location, bot):
+    # дождь
     if random.randint(1,10) == 1:
         for user in users:
-            bot.send_message(user['chat_id'], 'Пошел дождь')
+            bot.send_message(user['chat_id'], '☔️ Пошел дождь.')
             if 'wet' not in user['states']:
                 user['states'].append('wet')
+                bot.send_message(user['chat_id'], '💦 Вы намокли. Чтобы просохнуть, примите дождь.')
 
     hour = datetime.now().hour
 
@@ -90,8 +107,17 @@ def event(users, location, bot):
     if 0 < hour < 7:
         if random.randint(1, 3) == 1:
             for user in users:
-                bot.send_sticker(user['chat_id'], stickers['nikolay'])
-                bot.send_message(user['chat_id'], 'На улице появился Николай. Вы отвлекаете его от работы! Вы наказаны!')
-                user['states'].append('punishment')
+                if "punished" not in user['states']:
+                    bot.send_sticker(user['chat_id'], stickers['nikolay'])
+                    bot.send_message(user['chat_id'], '👺 В темноте появляется Николай, кажется он расстроен встречей с вами. Вы нарушаете правила и отвлекаете его от работы.\n😔 Вас отругали и вы расстроились. Вы возвращаетесь домой.')
+
+                    location = change_location_by_id(user, "home")
+                    try:
+                        location_module = importlib.import_module(location['file'])
+                        location_module.welcome(user, location, bot)
+                    except Exception as e:
+                        print(e)
+
+                    user['states'].append('punishment')
 
 
